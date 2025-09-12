a4d2dc03ea41eaf8a57ae9a598a603a9a877193f
kush <=>
2025-09-12 18:45:12 +0530

    supabase update

--- src/config/superbase storage/superbaseConfig.js
+++ src/config/superbase storage/superbaseConfig.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const { createClient } = require("@supabase/supabase-js");
-console.log(process.env.SuperbaseUrl);
 const superbase = createClient(
   process.env.SuperbaseUrl,
   process.env.SuperbaseKey
